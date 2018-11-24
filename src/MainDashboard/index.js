af0c6f744fe0e1c3cba8f2e07fe2c32fb1d8ef40
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-11-24 17:17:39 -0600

    first commit

---
+++ src/MainDashboard/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class MainDashboard extends Component{
+  constructor(){
+    super();
+    this.state ={
+      data: ''
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Main Page</h1>
+      </div>
+    )
+  }
+}
+
+export default MainDashboard;

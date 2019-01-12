4741fe3500e7ea6b2686b535d60bbf28b4dc72b6
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2019-01-11 20:23:57 -0600

    setup bootstrap and register page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import Register from './Register';
 import MainDashboard from './MainDashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>REact app</h1>
-        <MainDashboard />
+        <Switch>
+         <Route exact path="/" component={MainDashboard} />
+         <Route exact path="/register" component={Register} />
+       </Switch>
       </div>
     );
   }
---
+++ src/Register/index.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import {Form, Input} from 'reactstrap';
+import './style.css';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username:'',
+      password:''
+}
+  }
+
+  handleSubmit = async (e) => {
+  e.preventDefault();
+  const registerResponse = await fetch('http://localhost:9000/auth/register', {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(this.state),
+    headers:{
+      'Content-Type': 'application/json'
+      }
+  });
+  const parsedResponse = await registerResponse.json();
+    if(parsedResponse.data = 'register successful'){
+      // this.props.history.push('/profile');
+      //window.location.assign('http://localhost:3000')
+      this.props.history.push('/')
+  }
+}
+
+
+handleChange = (e) => {
+  this.setState({[e.target.name]:e.target.value});
+}
+
+  render() {
+    return(
+      <div> 
+        <div className='wrapper'>
+          <div className='loginForm'>
+            <h1 className='register'> Register </h1>
+              <Form onSubmit={this.handleSubmit}>
+                <input type='text' name='fullName' placeholder='full name' onChange={this.handleChange}/> <br/>
+                <input type='text' name='username' placeholder='username' onChange={this.handleChange}/><br/>
+                <input type='password' name='password' placeholder='password' onChange={this.handleChange}/>
+                <input className="registerButton" type='Submit' value='register'/>
+              </Form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Register;
